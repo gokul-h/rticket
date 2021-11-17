4f54a6e9d58afdf3180b2eede60d6120bb2faa33
gokul-h <72368205+gokul-h@users.noreply.github.com>
2021-11-17 13:11:21 +0530

    Added Testing

--- train/tests.py
+++ train/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class trainTest(TestCase):
+
+    # To check if correct template is rendered and url is accessible by name
+    def test_view_home_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'train/index.html')
+
+    def test_view_welcome_correct_template(self):
+        response = self.client.get(reverse('welcome'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'welcome.html')
+
+    def test_view_train_between_stations_correct_template(self):
+        response = self.client.get(reverse('train_between_stations'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'train/train_between_stations.html')
+
+    def test_view_train_info_correct_template(self):
+        response = self.client.get(reverse('train_info'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'train/train_info.html')
